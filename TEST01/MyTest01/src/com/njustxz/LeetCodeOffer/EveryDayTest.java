2e4baac2ed0354e22f4327bd58d8e8cb79dca730
HelloGit <1765474636@qq.com>
2021-07-26 14:50:45 +0800

    7.25 Daily problem

--- TEST01/MyTest01/src/com/njustxz/LeetCodeOffer/EveryDayTest.java
+++ TEST01/MyTest01/src/com/njustxz/LeetCodeOffer/EveryDayTest.java
@@ -1,5 +1,7 @@
 package com.njustxz.LeetCodeOffer;
 
+import java.util.*;
+
 public class EveryDayTest {
     public boolean find132pattern(int[] nums) {
         int n = nums.length;
@@ -36,6 +38,55 @@ public class EveryDayTest {
             }
         }
         return false;
+    }
+
+
+    //1743.从相邻元素还原数组
+    public static int[] restoreArray(int[][] adjacentPairs) {
+        if(adjacentPairs==null || adjacentPairs.length==0 ){
+            return null;
+        }
+        Map<Integer,Integer> numsPairs = new HashMap<>();
+        Set<Integer> head = new HashSet<>();
+        for (int[] adjacentPair : adjacentPairs) {
+            if(numsPairs.containsKey(adjacentPair[0])){
+                numsPairs.put(adjacentPair[1], adjacentPair[0]);
+            }else{
+                numsPairs.put(adjacentPair[0], adjacentPair[1]);
+            }
+
+            for (int j = 0; j < 2; j++) {
+                if (head.contains(adjacentPair[j])) {
+                    head.remove(adjacentPair[j]);
+                } else {
+                    head.add(adjacentPair[j]);
+                }
+            }
+        }
+        int n = adjacentPairs.length+1;
+        int[] ret = new int[n];
+        for(int num:head){
+            if(numsPairs.containsKey(num)){
+                ret[0]=num;
+                break;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            ret[i] = numsPairs.get(ret[i-1]);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = new int[3][2];
+        nums[0][0] = 2;
+        nums[0][1] = 1;
+        nums[1][0] = 3;
+        nums[1][1] = 4;
+        nums[2][0] = 3;
+        nums[2][1] = 2;
+        int[] ans = restoreArray(nums);
+        System.out.println(ans);
     }
 }
 
